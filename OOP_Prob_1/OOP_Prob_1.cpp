108624dc61d88ab9ad6a73a20137c33e90830e29
Bogdan <bogdan548584@gmail.com>
2024-02-23 18:00:40 +0200

    Добавьте файлы проекта.

---
+++ OOP_Prob_1/OOP_Prob_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int /*void*/ f(int arr[], int n)
+{ 
+    int y;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+        {
+            y = arr[i];
+        }
+        else
+        {
+            y = -arr[i];
+        }
+        /*cout << "Вiдповiдь:" << y << endl;*/
+    }
+    return y;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+
+    int size,A;
+    cout << "Введiть кiлькiсть чисел якi будуть в масивi" << endl;
+    cin >> size;
+    int *arr = new int[size];
+
+    cout << "Заповнiть масив" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> A;
+        arr[i] = A;
+    }
+    int result = f(arr, size); 
+    cout << "Повернене значення: " << result << endl; 
+    delete[] arr; 
+}
+
